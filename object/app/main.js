f0b32edc5ba400534aced722a7f5cfdae82a1c1e
jina <1490493025@qq.com>
2018-03-08 11:36:49 +0800

    no

--- object/app/main.js
+++
@@ -1,8 +0,0 @@
-(function(app){
-	document.addEventListener('DomContentLoaded',function(){
-		ng.platformBrowseerDynamic
-		  .platformBrowseerDynamic()
-		  .bootstrapModule(app.AppModule);
-
-	});
-})(window.app || (window.app{}));
